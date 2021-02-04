40ba07aa73420eb391ea3f4420c5bf5cd365b406
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-04 02:14:16 +0000

    edit 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: ");
+	printf("Size of a char: %c\n");
+	printf("Size of an int: %d\n");
+	printf("Size of a long int: %ld\n");
+	printf("Size of a long long in: %lld\n");
+	printf("Size of a float: %f\n");
         return (0);
 }
