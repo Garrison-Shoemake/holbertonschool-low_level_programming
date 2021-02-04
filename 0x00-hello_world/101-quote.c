485dddbe49336c366664ac409bd3ad72e6d936d6
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-04 05:24:20 +0000

    edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <errno.h>
 /**
  * main - runtime entry point
  * Return: return value 1
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	return (1);
 }
