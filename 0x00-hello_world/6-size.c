8475f970050f8c2bf4dfa109d6b8ded51adeb67b
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-04 02:28:53 +0000

    edit 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %c\n");
-	printf("Size of an int: %d\n");
-	printf("Size of a long int: %ld\n");
-	printf("Size of a long long in: %lld\n");
-	printf("Size of a float: %f\n");
-        return (0);
+	printf("Size of a char: %c\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a long int: %ld\n", sizeof(long int));
+	printf("Size of a long long int: %lld\n", sizeof(long long int));
+	printf("Size of a float: %f\n", sizeof(float));
+	return (0);
 }
