e53931eb34a4a65901a4a20ac2a870a8708a461f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-04 00:30:16 +0000

    add 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point of runtime
+ * Return: return value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
