d072c3cc53236487f6bbb9957f5cb1e2eda99d27
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-04 03:14:07 +0000

    add 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - runtime entry point
+ * Return: return value 1
+ */
+int main(void)
+{
+	echo("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
