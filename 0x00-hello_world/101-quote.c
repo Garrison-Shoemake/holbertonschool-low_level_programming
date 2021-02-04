f11e8da9a974e3977e7d87a01812ede109cbab69
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-04 04:46:30 +0000

    edit 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	>&2 echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19" 1 > &2;
 	return (1);
 }
