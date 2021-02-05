bf1ee97fedd8b61ee71806c1ed9145c226e900e5
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-05 22:38:57 +0000

    add 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - syntax start
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int i = 'z';
+
+		for ( ; i >= 'a'; i--)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+		return (0);
+}
