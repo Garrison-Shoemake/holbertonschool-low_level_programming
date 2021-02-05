f435affcaa9f743cb104e9eb67ebc933864cf9a8
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-05 21:46:06 +0000

    add 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- syntax start
+ * Return: return value
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	printf("%i", i);
+	printf("\n");
+			return (0);
+}
