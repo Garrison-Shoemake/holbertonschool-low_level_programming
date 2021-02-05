8d66a09e60628d36086e272c4bf6c21030cf89a7
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-05 05:46:38 +0000

    edit 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - styntax start
+ * Return: return value
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'q')(ch != 'e');
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
