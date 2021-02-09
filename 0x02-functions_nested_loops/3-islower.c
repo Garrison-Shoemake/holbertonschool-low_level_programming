63b93c22765a6246fdf781240ea22a2b6d3cf636
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-09 22:19:58 +0000

    edit 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 /**
  * _islower - will retun values based on case of return value
- *
+ * Return: will return 1 if lower case and 0 if otherwise
+ * int c - determines retrun value
  */
 int _islower(int c)
 {
