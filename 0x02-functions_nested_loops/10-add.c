c4f8c8fe494c995cf35ea86be7d2e5bb83894190
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-10 05:50:18 +0000

    edit 10 / header

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
 /**
- *
- *
+ * add - function prototype
+ * @a: first int
+ * @b: second int
+ * Return: return value
  */
+
 int add(int a, int b)
 {
-	return (a+b);
+	return (a + b);
 }
