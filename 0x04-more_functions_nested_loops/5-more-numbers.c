307641de7737130cdc8a06d20b06a42ac6467dcf
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-11 21:29:19 +0000

    edit 5

--- 0x04-more_functions_nested_loops/5-more-numbers.c
+++ 0x04-more_functions_nested_loops/5-more-numbers.c
@@ -11,9 +11,12 @@ void more_numbers(void)
 
 	for (y = 0; y < 10; y++)
 	{
-	for (x = 0; x < 15; x++)
+	for (x = 0; x <= 14; x++)
 	{
+		if (x > 9)
+		{
 		_putchar(x / 10 + '0');
+		}
 		_putchar(x % 10 + '0');
 	}
 	_putchar('\n');
