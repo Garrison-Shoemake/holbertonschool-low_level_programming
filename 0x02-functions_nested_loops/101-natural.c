a4a7dd30f5492e4da37f19a809089318b54c2632
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-11 01:31:53 +0000

    edit 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,12 +3,7 @@
 
 /**
  * main - syntax start
- * @a: integer for 3 part of problem
- * @b: integer for 5 part of problem
- * @sum: sum of 3 into 1024
- * @sum2: sum of 5 into 1024
  * Return: return value
- *
  */
 
 int main(void)
@@ -16,12 +11,12 @@ int main(void)
 	int a;
 	int b;
 	int sum = 0;
-	int sum2= 0;
+	int sum2 = 0;
 
 	for (a = 3; a <= 1024; a += 3)
 		sum += a;
 
-	for (b = 5; b <=1024; b += 5)
+	for (b = 5; b <= 1024; b += 5)
 		sum2 += b;
 
 	printf("%d\n", sum + sum2);
