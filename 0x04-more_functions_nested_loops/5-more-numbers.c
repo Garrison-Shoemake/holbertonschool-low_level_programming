86b77e6da150d6de9de2b5d9c7121d2f83ef68d5
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-11 21:16:10 +0000

    edit 5

--- 0x04-more_functions_nested_loops/5-more-numbers.c
+++ 0x04-more_functions_nested_loops/5-more-numbers.c
@@ -7,11 +7,16 @@
 void more_numbers(void)
 {
 	int x;
+	int y;
 
+	for (y = 0; y < 10; y++)
+	{
 	for (x = 0; x < 15; x++)
 	{
 		_putchar(x / 10 + '0');
 		_putchar(x % 10 + '0');
 	}
 	_putchar('\n');
+	}
+
 }
