ca908097cf6f6a7c5628b34bfd89b2e605f4abfe
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-11 20:53:01 +0000

    add 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - syntax start of function
+ *
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
