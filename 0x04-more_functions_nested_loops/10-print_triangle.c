d6cca6681954bbc21ac2a5c8984166f1ca22d49c
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-12 01:01:35 +0000

    add 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+	int c;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = size; b > a; b--)
+			{
+				_putchar(' ');
+			}
+			for (c = 1; c <= a; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
