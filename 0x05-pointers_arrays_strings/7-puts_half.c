e346e1e093dfdd1a4bda62e440ca84990402c496
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-17 23:26:31 +0000

    add 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * puts_half - function name
+ * @str: string variable
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	int n;
+	int length;
+
+	n = 0;
+
+	while (str[length] != '\0')
+        {
+                length++;
+        }
+
+	if ( n % 2 == 0)
+	{
+		for (n = length / 2; n < length; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		_putchar(str[n+1]);
+	}
+		_putchar('\n');
+
+}
