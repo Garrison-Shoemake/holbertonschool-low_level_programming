aeca5261c8fb2a7bfdda21adbb3ed22692f11b9e
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-18 22:06:11 +0000

    add 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strcmp - function name
+ * @s1: first pointer
+ * @s2: second pointer
+ * Return: return value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1)
+	{
+		if (s1 != s2)
+			break;
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
