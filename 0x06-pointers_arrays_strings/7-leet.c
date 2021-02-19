ff7cc9a7ebd8c5091d5cbba72c61ef89f4ad69f7
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-19 05:41:02 +0000

    add 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * leet - function name
+ * @str: string variable
+ * Return: returns
+ *
+ */
+char *leet(char *str)
+{
+	int i = 0;
+	int n = 0;
+	char let[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int num[] = {'4', '3', '0', '7', '1'};
+
+	while (str[i] != '\0')
+	{
+		for (n = 0; n <= 9 ; n++)
+		{
+			if (str[i] == let[n] && let[n] != num[n])
+			{
+				str[i] = num[n];
+			}
+		}
+		i++;
+	}
+	return (str);
+}
