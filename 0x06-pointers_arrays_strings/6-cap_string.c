7640b13e36d2b6589cc57139a079e16d0633cb05
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-19 04:10:32 +0000

    add 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * cap_string - function name
+ * @str: character string
+ * Return: return value
+ *
+ *
+ */
+char *cap_string(char *str)
+{
+	int n = 0;
+	int m = 0;
+	char sep[] = {9, ' ', 10, ',', ';', '.', '!', '?', '\"', '(', '{', '}', ')'};
+
+	while (str[n] != '\0')
+	{
+		for (m = 0; m <= 12; m++)
+		{
+			if (str[n] == sep[m])
+			{
+				if (str[n + 1] >= 'a' && str[n + 1] <= 'z')
+				{
+					str[n + 1] = str[n + 1] - 32;
+				}
+			}
+		}
+		n++;
+	}
+	return (str);
+}
