110e17ec655977421a0885adeae4f8d9505e8f3c
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-23 03:12:26 +0000

    add 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strstr - function name
+ * @haystack: haystack we're looking in
+ * @needle: needle we're finding
+ * Return: return value
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int n = 0;
+	int m;
+
+	while (haystack[n] != '\0')
+	{
+		n = 0;
+		if  (haystack[n] == needle[m])
+		{
+			m++;
+			n++;
+		}
+		else
+		{
+			return (needle);
+		}
+	}
+	return ('\0');
+}
