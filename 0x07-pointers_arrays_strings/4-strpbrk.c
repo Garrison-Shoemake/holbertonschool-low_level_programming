3428de950ba1ee2e5ee445258660fa2a97082969
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-23 02:19:21 +0000

    edit 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,36 +9,19 @@ char *_strchr(char *s, char c);
 
 char *_strpbrk(char *s, char *accept)
 {
+	int i = 0;
 
 	if ((s == '\0') || (accept == '\0'))
 		return ('\0');
 
 	while (*s)
 	{
-		if (_strchr(accept, *s))
-			return (s);
-
-		else
-			s++;
-	}
-	return ('\0');
-}
-
-/**
- * _strchr - function name
- * @s: string to check
- * @c: what we are checking the string for
- * Return: return value
- */
-char *_strchr(char *s, char c)
-{
-	int n = 0;
-
-	while (s[n] >= '\0')
-	{
-		if (s[n] == c)
-			return (s + n);
-		n++;
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
 	}
 	return ('\0');
 }
