2f302361c1a5949f86627f109ee48ebbfee82566
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-25 03:41:27 +0000

    add 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+int prime(int j, int i);
+/**
+ * is_prime_number - main function name
+ * @n: variable from main
+ * Return: calls helper func for output
+ *
+ */
+int is_prime_number(int n);
+{
+	return (prime(n, 0));
+}
+/**
+ * prime - function name
+ * @i: passed int from first func
+ * @j: incrementation number
+ * Return: return statements for determining
+ *
+ */
+int prime(int j, int i)
+{
+	if (i < 2)
+		return (0);
+	if (j % i == 0)
+		return (0);
+	if (i > (j / 2))
+		return (1);
+	return (prime(j, (i + 1)));
+}
