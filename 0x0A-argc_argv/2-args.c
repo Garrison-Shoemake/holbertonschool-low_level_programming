75cd3467afa8a976d1fca2edce83ae1b5e3ea799
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-28 19:52:57 +0000

    add 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - main function name
+ * @argc: number of arguments
+ * @argv: arguments to main
+ * Return: return value
+ */
+int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
+{
+	int count = 0;
+
+
+	while (count < argc)
+	{
+		printf("%s\n", argv[count]);
+		count++;}
+		return (0);
+}
