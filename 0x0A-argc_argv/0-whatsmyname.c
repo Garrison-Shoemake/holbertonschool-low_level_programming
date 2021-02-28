4b0f9ee5f16e793c593a4274f344fde2af29d862
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-02-28 01:25:57 +0000

    edit 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * main - main function
+ * @argc: count of arguments
+ * @argv: vector, or agruments made to main
+ * Return: return value, end of function
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return;
+	return (0);
 }
