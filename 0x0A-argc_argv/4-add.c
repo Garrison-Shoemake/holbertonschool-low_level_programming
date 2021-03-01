f1257d036e33f4f68a3990cfcc1c065bba7f1066
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-01 01:18:19 +0000

    edit 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -24,8 +24,12 @@ int main(int argc, char **argv)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0)
+
+			if (atoi(argv[i]) >= '0' && atoi(argv[i]) <= '9')
 			{
+
+
+
 				printf("Error\n");
 				return (1);
 			}
