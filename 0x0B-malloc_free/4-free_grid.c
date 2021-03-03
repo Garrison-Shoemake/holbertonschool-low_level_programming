963b97d9f486316080987ebda5b6e61aad2805fc
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-03 05:16:08 +0000

    edit 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-
 /**
  * free_grid - function name
  * @grid: grid to free
@@ -11,5 +10,5 @@ void free_grid(int **grid, int height)
 {
 	(void)height;
 	free(grid);
-	return;
+
 }
