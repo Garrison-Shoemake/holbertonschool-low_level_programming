32cde1cdef8cfc4b3e1ecebad2da7befcbe8bca7
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-03 04:52:02 +0000

    add 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,54 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
-/**
- * alloc_grid - function name
- * @width: width of the array
- * @height: heigh of the array
- * Return: return value
- */
-int **alloc_grid(int width, int height)
-{
-        int **grid;
-        int i;
-        int j;
-        int k;
 
-        if (width <= 0)
-        {
-                return (NULL);
-        }
-        if (height <= 0)
-        {
-                return (NULL);
-        }
-
-        grid = malloc(height * sizeof(int *));
-
-        if (grid == NULL)
-        {
-                return (NULL);
-        }
-
-
-        for (i = 0; i < height; i++)
-        {
-                grid[i] = malloc(width * sizeof(int));
-
-                if (grid[i] == NULL)
-                {
-                        return (NULL);
-                }
-        }
-
-        for (j = 0; j < height ; j++)
-        {
-                for (k = 0; k < width  ; k++)
-                {
-                        grid[j][k] = 0;
-                }
-        }
-        return (grid);
-}
 /**
  * free_grid - function name
  * @grid: grid to free
@@ -58,5 +10,4 @@ int **alloc_grid(int width, int height)
 void free_grid(int **grid, int height __attribute__((unused)))
 {
 	free(grid);
-	return;
 }
