f1e31a4cfa4332b2c84f3724f507511810c67196
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-05 03:16:51 +0000

    edit 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,12 +12,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	unsigned int j = nmemb * size;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
-		return (NULL);
-
-	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	for (i = 0; i < j; i++)
