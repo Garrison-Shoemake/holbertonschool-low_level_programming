de1c25d0cc5d682c2fc9a1fc677c45fe6be67be1
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-05 02:28:50 +0000

    add 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - function name. calls for an array of num elements of size bytes
+ * @nmemb: number of elements for calloc
+ * @size: size of elements in array
+ * Return: returns pointer to location in heap
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	char *ptr;
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	return (ptr);
+}
