eb5f8d3e24a7c22894bd95fa8009a12a6b1a70fa
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-05 04:34:19 +0000

    add 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * array_range - function name, contains values from min to max
+ * @min: start of array
+ * @max: end of array
+ * Return: return value
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+	int j = 0;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc((max - min) * sizeof(int) + (1 * sizeof(int)));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+	{
+		ptr[j] = i;
+		j++;
+	}
+
+	return(ptr);
+}
