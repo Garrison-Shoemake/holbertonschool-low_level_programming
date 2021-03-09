2b72f09a72fb664218b5bb0e01f882eb33d73569
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-09 20:19:50 +0000

    add 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_dog - function name
+ * @d: struct to free
+ */
+void free_dog(dog_t *d)
+{
+	free ((*d).name);
+	free ((*d).owner);
+	free (d);
+}
