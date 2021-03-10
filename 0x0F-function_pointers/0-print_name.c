c525c293a54581c0884209a1aed556f25ce426e8
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-10 19:56:50 +0000

    add 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - name of the function
+ * @name: name to print
+ * @f: function to call
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+}
