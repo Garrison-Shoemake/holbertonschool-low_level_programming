1d9fc9b4cb78f16426ab288a932165fe3af8746f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-10 20:42:30 +0000

    edit 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * array_iterator - function name
+ * @array: array to iterate through
+ * @size: size of the array
+ * @action: function to call
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
