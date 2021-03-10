af5dc0d89146ea2af134272ea6174fef27013bac
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-10 20:10:45 +0000

    edit 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - name of the function
  * @name: name to print
@@ -6,7 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 
 }
