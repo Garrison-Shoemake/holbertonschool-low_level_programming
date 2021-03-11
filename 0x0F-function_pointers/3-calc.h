6b38dd47ff23a3ac6ad1c045d35cfc4c64a50724
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-11 04:25:43 +0000

    edit 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,7 @@
 #ifndef CALC_H
 #define CALC_H
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct op - Struct op
  * @op: The operator
@@ -17,7 +18,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int main(int argc, char *argv[]);
 int (*get_op_func(char *s))(int, int);
 
 #endif
