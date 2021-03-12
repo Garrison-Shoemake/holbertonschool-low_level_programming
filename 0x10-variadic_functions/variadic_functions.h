df0fadb8b3fcdca23281db4dfdb8f88be01f1303
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-12 19:30:54 +0000

    edit header and 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+
+/**
+ * struct format_specifiers - struct name
+ * @s: string to check
+ * @f: function to call
+ */
+typedef struct format_specifiers
+{
+	char *s;
+	void (*f)();
+} format_t;
+
 #endif
