45b4e198cc881f35b049c410f805d7fc94894093
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-18 22:22:49 +0000

    edit 1 for betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 /**
+ * list_len - function name, counts the lists
+ * @h: list to check and count
+ * Return: returns the count of lists
  */
 size_t list_len(const list_t *h)
 {
 	size_t i = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		h = h->next;
 		i++;
