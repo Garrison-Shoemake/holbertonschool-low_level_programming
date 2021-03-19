b24c3210b8a3eb67bd887b30a17c69ec510ae63f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-19 00:02:20 +0000

    edit 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 #include <string.h>
 /**
+ * *add_node - function name, adds a node to the beginning of the list
+ * @head: start of the list, put new list before here
+ * @str: string to be added and printed via our print_list
+ * Return: returns pointer to the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -10,6 +14,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new == NULL)
 		return (NULL);
+
 
 	new->str = strdup(str);
 	new->next = *head;
