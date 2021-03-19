025b2b35c06490655e01472a6c8bf5af10f99bd8
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-19 00:36:24 +0000

    add task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,8 @@
 #include "lists.h"
 #include <string.h>
+int _strlen(char *str);
+
+
 /**
  * *add_node - function name, adds a node to the beginning of the list
  * @head: start of the list, put new list before here
@@ -10,16 +13,41 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
+	if (head == NULL || str == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(*head));
 
 	if (new == NULL)
 		return (NULL);
 
-
 	new->str = strdup(str);
+
+	if (new->str == NULL)
+	{
+		free (new);
+		return (NULL);
+	}
+
+	new->len = _strlen(new->str);
 	new->next = *head;
 	*head = new;
 
 
 	return (new->next);
+}
+/**
+ * _strlen - function name, counts a string
+ * @str: string to check
+ * Return: returns length
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
