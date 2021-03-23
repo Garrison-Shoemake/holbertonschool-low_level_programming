a8b05d12163c0fb4a76387d2cd34bb400f647f43
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-23 21:32:53 +0000

    worth a shot?

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - function name, frees the lists we've made so far
+ * @head: head of the lists to free
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
