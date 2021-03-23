7125a4b4791f51c0553f2afc72afef78d263fd7d
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-23 22:40:05 +0000

    edit 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,9 +1,14 @@
 #include "lists.h"
 /**
+ * free_listint2 - function name, frees a null head
+ * @head: head of the list to free
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
+
+	if (!head)
+		return;
 
 	while (*head)
 	{
