68c300ecd0dca79e2eab47ff7db07155a38a6917
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-23 21:27:08 +0000

    add null check

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,6 +14,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
