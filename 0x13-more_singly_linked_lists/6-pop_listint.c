75cab89fbe87ee01df2f1933a5b3a32e1416f56b
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-24 01:15:44 +0000

    betty edits... beddits?

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,8 @@
 #include "lists.h"
 /**
+ * pop_listint - function name, deletes the first node and returns it's value
+ * @head: beginning of the linked list to check
+ * Return: returns the value of n before deleting node
  */
 int pop_listint(listint_t **head)
 {
