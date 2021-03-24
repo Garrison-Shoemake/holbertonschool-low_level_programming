7348891138ec9a925a6b29d29b1d19bcc569773c
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-24 02:02:57 +0000

    edit 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,11 +14,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (head)
 	{
-		for (i = 0; i < index; i++)
-			head = head->next;
-		if (!head)
+		if (head == NULL)
 			return (NULL);
-		return (head);
-	}
+		else
+		{
+			for (i = 0; i < index; i++)
+				head = head->next;
+			return (head);
+		}
+}
 	return (NULL);
 }
