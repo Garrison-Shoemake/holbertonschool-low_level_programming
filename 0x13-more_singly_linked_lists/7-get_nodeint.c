e457350557f6509481ca4b260240c9723cd42864
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-24 01:39:16 +0000

    add 7 after bedits

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - function name, gets information from the node
+ * @head: head of the linked list to go through
+ * @index: index to retieve from
+ * Return: return index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	while (head)
+	{
+		for (i = 0; i <= index; i++)
+			head = head->next;
+
+		return (head);
+	}
+	return (NULL);
+}
