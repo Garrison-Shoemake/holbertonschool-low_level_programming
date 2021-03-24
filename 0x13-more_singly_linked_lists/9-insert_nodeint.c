bfcdf346886f86ac12ec3a0f52478e5e763e9ce2
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-24 04:32:43 +0000

    edit 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,11 +23,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (start == NULL)
 		return (NULL);
 
-	if (idx == 0)
-	{
-		
-	}
-
 	start = *head;
 
 	tmp->n = n;
