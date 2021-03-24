7e5d9e39d72171eb66b6daf9bc2da2d5bb97ab16
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-24 01:53:17 +0000

    edit 7?

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,13 +9,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
+
 	while (head)
 	{
 		for (i = 0; i < index; i++)
 			head = head->next;
 
-		if (head == NULL)
+		if (!head->n)
 			return (NULL);
+
 		return (head);
 	}
 	return (NULL);
