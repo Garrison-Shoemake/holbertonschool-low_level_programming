6bd741834586d9412e73a8732d12567368295e7d
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-31 03:06:28 +0000

    edit 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		return(-1);
+		return (1);
 
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
