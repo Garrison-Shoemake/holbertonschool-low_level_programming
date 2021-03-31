bf99440f7cd92ed1a384e0fb049c54fe19841b37
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-31 04:46:13 +0000

    add 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+/**
+ * cp - function name, copies from one file to another
+ * @argc: number of arguments, always 3, 0 - cp, 1 from, 2 - to
+ * @argv: arguments as names to copy file from and to
+ * Return: return 0 on success, exit if fail
+ */
+int cp(int argc, char *argv[])
+{
+	int from, to, wr, rd;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	from = open(argv[1], O_RDWR);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	to = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	rd = read(from, buffer, 1024);
+	if (rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	wr = write(argv[1], argv[2], buffer);
+	if (wr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (close(from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", from);
+		exit(100);
+	}
+	if (close(to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", to);
+		exit(100);
+	}
+	return (0);
+}
