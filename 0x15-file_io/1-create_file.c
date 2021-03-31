417ab5f0c0c8910cb6314afb7ad8d30dc3fd76b6
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-31 02:57:51 +0000

    bedits and checks?

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,19 +7,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0;
+	int fd, len = 0, w;
 
 	if (!filename)
-		return(-1);
+		return (-1);
 
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 
-	while (!text_content)
+	while (text_content[len] != '\0')
 		len++;
 
-	write(fd, text_content, len);
-	close (fd);
-	return(1);
+	w = write(fd, text_content, len);
+	if (w == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
 }
