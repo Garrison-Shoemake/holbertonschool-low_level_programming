e795badcff55aa165049509f3674ebd22853370b
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-31 02:49:06 +0000

    add 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * create_file - function name, creates a file
+ * @filename: filename to create
+ * @text_content: null terminates string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0;
+
+	if (!filename)
+		return(-1);
+
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+
+	while (!text_content)
+		len++;
+
+	write(fd, text_content, len);
+	close (fd);
+	return(1);
+}
