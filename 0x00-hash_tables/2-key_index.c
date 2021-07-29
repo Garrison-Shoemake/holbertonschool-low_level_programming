7b746aa50db07c6e8548177c112bba5659c0a5af
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-29 22:02:58 +0000

    oops betty

--- 0x00-hash_tables/2-key_index.c
+++ 0x00-hash_tables/2-key_index.c
@@ -4,8 +4,9 @@
 * key_index - find a key's index
 * @key: key to index
 * @size: size of array of hash table
+* Return: returns index where to place
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return (hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
