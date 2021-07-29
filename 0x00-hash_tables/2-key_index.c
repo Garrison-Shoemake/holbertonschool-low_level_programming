c3cb7b47bf955ee17b2202ba38c019b3b6134d54
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-29 22:01:26 +0000

    add task 2

---
+++ 0x00-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+/**
+* key_index - find a key's index
+* @key: key to index
+* @size: size of array of hash table
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    return (hash_djb2(key) % size);
+}
