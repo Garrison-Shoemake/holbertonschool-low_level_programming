c0f2e4d062c17c226aa439e462b2dd94f00c6a2e
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-29 21:26:14 +0000

    add task 1 files

--- 0x00-hash_tables/1-djb2.c
+++ 0x00-hash_tables/1-djb2.c
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 /**
-* @str: string to 
-*
+* hash_djb2 - funciton to hash in djb2 style
+* @str: string to hash
+* Return: returns the new hash
 */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
