df7470b87fc037159bb5768cfbfc0e92d15b0c0a
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-30 00:12:43 +0000

    add typecasting to function call

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || strlen(key) == 0)
 		return (0);
 
-	index = key_index(key, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
 	current = malloc(sizeof(hash_node_t));
 
 	if (current == NULL)
