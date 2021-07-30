b8b4c3859a3863cc2435da43f6ae85bc57014367
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-30 00:45:03 +0000

    add task 4

---
+++ 0x00-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+* hash_table_get - retreives value with a key
+* @ht: hash table to search
+* @key: key to find in a hash table
+* Return: returns value of found elemtn
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    unsigned long int index = 0;
+    hash_node_t *current;
+
+    if (ht == NULL || key == NULL)
+        return (NULL);
+
+    index = key_index((unsigned char *)key, ht->size);
+    current = ht->array[index];
+    if (current == NULL)
+        return (NULL);
+    while (strcmp(key, current->key) != 0)
+        current = current->next;
+
+    return (current->value);
+}
