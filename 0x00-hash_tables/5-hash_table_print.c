fc5eafc72e753cc6c2ffe91778f47676b43c6e2a
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-30 01:26:58 +0000

    add task 5??? betty please???

---
+++ 0x00-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+* hash_table_print - funciton to hash in djb2 style
+* @ht: string to hash
+*
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *current;
+	int check = 0;
+
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		current = ht->array[index];
+		while (current != NULL)
+		{
+			if (check)
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			current = current->next;
+			check = 1;
+		}
+	}
+	printf("}\n");
+}
