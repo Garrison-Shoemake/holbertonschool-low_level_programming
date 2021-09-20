c89ee0aca07ec88c684fd8a28bc47ecc962f435f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-09-20 22:18:29 +0000

    add task 0

---
+++ 0x02-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: array to search
+ * @size: size of the array
+ * @value: the value in the array to find
+ * Return: returns the index at found value, otherwise -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+	while (i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			printf("Found %d at index: %ld\n", value, i);
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
